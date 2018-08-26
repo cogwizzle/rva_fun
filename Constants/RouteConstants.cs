49476d5770a9d9eb51b158b258b0261556d4c9ed
Cody Halbleib <chalb500@gmail.com>
2018-08-26 10:00:27 -0400

    fixed the activity conflict; moved routing strings to RouteConstants

---
+++ Constants/RouteConstants.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Constants
+{
+  public static class RouteConstants {
+    public static string apiRoot = "http://ec2-52-91-254-158.compute-1.amazonaws.com/";
+    public static string apiActivities = apiRoot + "activities/";
+  }
+}
